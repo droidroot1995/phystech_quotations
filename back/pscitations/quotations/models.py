310526766280e3edb2330aa9ed43588cc7378aea
droidroot <goryakin@phystech.edu>
2020-07-16 15:35:32 +0300

    Backend models added

---
+++ back/pscitations/quotations/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Quotation(models.Model):
+    author = models.TextField(max_length=64)
+    quot_text = models.TextField(max_length=1024)
+    added_at = models.DateTimeField(null=True, auto_now_add=True)
+    added_by = models.ForeignKey(to='users.User', on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = 'Цитата'
+        verbose_name_plural = 'Цитаты'
+        ordering = ['added_at']
