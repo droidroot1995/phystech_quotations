5f04ea0290be2602f39cf700689da8b4549ff742
droidroot <goryakin@phystech.edu>
2020-07-17 15:30:23 +0300

    models fix

--- back/pscitations/quotations/admin.py
+++ back/pscitations/quotations/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from quotations.models import Quotation
 
 class QuotationAdmin(admin.ModelAdmin):
-    list_display = ('author', 'quotation')
+    list_display = ('author', 'quot_text')
     
 admin.site.register(Quotation, QuotationAdmin)
